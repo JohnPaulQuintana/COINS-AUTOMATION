120a6a2749d761e98093ecce25b156429d6cd6df
ExousiaNavi <jpquintana2024@gmail.com>
2026-07-19 11:01:56 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 // import PriceTable from "./components/PriceTable";
 import TrendDashboard from "./components/TrendDashboard";
 
+console.log = () => {};
+console.info = () => {};
+console.warn = () => {};
+console.error = () => {};
+console.debug = () => {};
 
-function App(){
-
-    return (
-        <div className="custom-scroll">
-            <TrendDashboard/>
-        </div>
-    )
-
+function App() {
+  return (
+    <div className="custom-scroll">
+      <TrendDashboard />
+    </div>
+  );
 }
-
 
 export default App;
